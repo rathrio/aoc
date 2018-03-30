c079fca142da8c3871c6adabe6c38806675be9e6
Rathesan Iyadurai <rad.iyadurai@gmail.com>
2018-03-30 16:51:25 +0200

    Add 18-2

--- 2017/ruby/18-2.rb
+++ 2017/ruby/18-2.rb
@@ -1,48 +1,83 @@
 #!/usr/bin/env ruby
 
-registers = {}
-registers.default = 0
-frequencies = []
+require 'thread'
+
+class Program
+  def initialize(id, receive:, send:)
+    @id = id
+    @receive = receive
+    @send = send
+    @registers = { 'p' => @id }
+    @registers.default = 0
+    @send_count = 0
+  end
+
+  def run(instructions)
+    Thread.new do
+      i = 0
+
+      while i < instructions.length
+        instruction = instructions[i]
+        op, x, y = instruction.split
+
+        if y
+          y = if y.to_s =~ /^[a-z]$/
+                @registers[y]
+              else
+                y.to_i
+              end
+        end
+
+        case op
+        when "snd"
+          x = (x.to_s =~ /^[a-z]$/) ? @registers.fetch(x) : x.to_i
+          @send << x
+          @send_count += 1
+          puts @send_count if @id == 1
+        when "set"
+          @registers[x] = y
+        when "add"
+          @registers[x] += y
+        when "mul"
+          @registers[x] *= y
+        when "mod"
+          @registers[x] %= y
+        when "rcv"
+          @registers[x] = @receive.pop
+        when "jgz"
+          x = (x.to_s =~ /^[a-z]$/) ? @registers.fetch(x) : x.to_i
+          if x > 0
+            i += y 
+            next
+          end
+        end
+
+        i += 1
+      end
+    end
+  end
+end
 
 instructions = ARGF.readlines.map(&:chomp)
 
-i = 0
+p0_receive_queue = Queue.new
+p1_receive_queue = Queue.new
 
-while i < instructions.length
-  instruction = instructions[i]
-  op, x, y = instruction.split
+p0 = Program.new(
+  0,
+  receive: p0_receive_queue,
+  send: p1_receive_queue
+).run(instructions)
 
-  if y
-    y = if y.to_s =~ /^[a-z]$/
-      registers[y]
-    else
-      y.to_i
-    end
-  end
+p1 = Program.new(
+  1,
+  receive: p1_receive_queue,
+  send: p0_receive_queue
+).run(instructions)
 
-  case op
-  when "snd"
-    frequencies << registers.fetch(x)
-  when "set"
-    registers[x] = y
-  when "add"
-    registers[x] += y
-  when "mul"
-    registers[x] *= y
-  when "mod"
-    registers[x] %= y
-  when "rcv"
-    unless registers.fetch(x).zero?
-      puts frequencies.last 
-      return
-    end
-  when "jgz"
-    if registers.fetch(x) > 0
-      i += y 
-      next
-    end
-  end
-
-  i += 1
+begin
+  p0.join
+  p1.join
+rescue Exception
+  exit
 end
-
