05db861ce9bcb4ca1e4196b95a2defd0375a6c75
Rathesan Iyadurai <rad.iyadurai@gmail.com>
2018-12-15 22:55:34 +0100

    Fix movement

--- 2018/rust/day13/src/main.rs
+++ 2018/rust/day13/src/main.rs
@@ -1,6 +1,6 @@
 use std::env;
 
-#[derive(Debug)]
+#[derive(Debug, Copy, Clone)]
 enum Direction {
     Up,
     Down,
@@ -8,70 +8,101 @@ enum Direction {
     Right,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Copy, Clone)]
 struct Cart {
     position: (usize, usize),
     direction: Direction,
+    intersection_choice: u8,
 }
 
 impl Cart {
-    fn next_position(&self) -> (usize, usize) {
-        let (x, y) = self.position;
-
-        match self.direction {
-            Direction::Up => (x, y - 1),
-            Direction::Down => (x, y + 1),
-            Direction::Left => (x - 1, y),
-            Direction::Right => (x + 1, y),
+    fn new(position: (usize, usize), direction: Direction) -> Cart {
+        Cart {
+            position,
+            direction,
+            intersection_choice: 1,
         }
     }
 
-    fn update_position(&mut self, position: (usize, usize)) {
-        self.position = position;
+    fn mv(&mut self) -> (usize, usize) {
+        let (x, y) = self.position;
+
+        match self.direction {
+            Direction::Up => self.position = (x, y - 1),
+            Direction::Down => self.position = (x, y + 1),
+            Direction::Left => self.position = (x - 1, y),
+            Direction::Right => self.position = (x + 1, y),
+        };
+
+        self.position
     }
 
     fn update_direction(&mut self, direction: Direction) {
         self.direction = direction;
     }
 
+    fn turn_left(&mut self) {
+        match self.direction {
+            Direction::Up => self.direction = Direction::Left,
+            Direction::Down => self.direction = Direction::Right,
+            Direction::Left => self.direction = Direction::Down,
+            Direction::Right => self.direction = Direction::Up,
+        }
+    }
+
+    fn turn_right(&mut self) {
+        match self.direction {
+            Direction::Up => self.direction = Direction::Right,
+            Direction::Down => self.direction = Direction::Left,
+            Direction::Left => self.direction = Direction::Up,
+            Direction::Right => self.direction = Direction::Down,
+        }
+    }
+
     fn choose_direction(&mut self) {
+        match self.intersection_choice {
+            1 => {
+                self.turn_left();
+                self.intersection_choice = 2;
+            }
+            2 => {
+                self.intersection_choice = 3;
+            }
+            3 => {
+                self.turn_right();
+                self.intersection_choice = 1;
+            }
+            _ => (),
+        }
     }
 }
 
 fn parse_grid() -> (Vec<Vec<char>>, Vec<Cart>) {
     let contents = include_str!("input.txt");
     let mut cells: Vec<Vec<char>> = vec![vec![' '; 150]; 150];
+
+    // let contents = include_str!("sample.txt");
+    // let mut cells: Vec<Vec<char>> = vec![vec![' '; 13]; 6];
+
     let mut carts: Vec<Cart> = Vec::new();
 
     for (y, line) in contents.lines().enumerate() {
         for (x, c) in line.chars().enumerate() {
             cells[y][x] = match c {
                 '>' => {
-                    carts.push(Cart {
-                        position: (x, y),
-                        direction: Direction::Right,
-                    });
+                    carts.push(Cart::new((x, y), Direction::Right));
                     '-'
                 }
                 '<' => {
-                    carts.push(Cart {
-                        position: (x, y),
-                        direction: Direction::Left,
-                    });
+                    carts.push(Cart::new((x, y), Direction::Left));
                     '-'
                 }
                 '^' => {
-                    carts.push(Cart {
-                        position: (x, y),
-                        direction: Direction::Up,
-                    });
+                    carts.push(Cart::new((x, y), Direction::Up));
                     '|'
                 }
                 'v' => {
-                    carts.push(Cart {
-                        position: (x, y),
-                        direction: Direction::Down,
-                    });
+                    carts.push(Cart::new((x, y), Direction::Down));
                     '|'
                 }
                 _ => c,
@@ -86,31 +117,50 @@ fn part1() {
     let (grid, mut carts) = parse_grid();
 
     loop {
-        for cart in carts.iter_mut() {
-            let next_position = cart.next_position();
-            cart.update_position(next_position);
+        carts.sort_by(|c1, c2| {
+            let (c1_x, c1_y) = c1.position;
+            let (c2_x, c2_y) = c2.position;
 
-            let (next_x, next_y) = next_position;
-            let next_cell = grid[next_y][next_x];
-            match next_cell {
+            (c1_y, c1_x).cmp(&(c2_y, c2_x))
+        });
+
+        for cart in carts.iter_mut() {
+            let (x, y) = cart.position;
+            let cell = grid[y][x];
+            match cell {
                 '/' => {
                     match cart.direction {
                         Direction::Up => cart.update_direction(Direction::Right),
+                        Direction::Down => cart.update_direction(Direction::Left),
                         Direction::Left => cart.update_direction(Direction::Down),
-                        _ => (),
+                        Direction::Right => cart.update_direction(Direction::Up),
                     };
                 }
                 '\\' => {
                     match cart.direction {
                         Direction::Up => cart.update_direction(Direction::Left),
+                        Direction::Down => cart.update_direction(Direction::Right),
                         Direction::Right => cart.update_direction(Direction::Down),
-                        _ => (),
+                        Direction::Left => cart.update_direction(Direction::Up),
                     };
                 }
                 '+' => {
                     cart.choose_direction();
                 }
                 _ => (),
+            }
+
+            let new_position = cart.mv();
+            let cart_positions: Vec<(usize, usize)> = carts.iter().map(|c| c.position).collect();
+            if cart_positions
+                .iter()
+                .filter(|p| **p == new_position)
+                .count()
+                > 1
+            {
+                let (x, y) = new_position;
+                println!("{},{}", x, y);
+                break;
             }
         }
     }
