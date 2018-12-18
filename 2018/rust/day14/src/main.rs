83cf18ad53d28e73cea297ae89953f325e0fba1d
Rathesan Iyadurai <rad.iyadurai@gmail.com>
2018-12-18 23:43:27 +0100

    Add day14 part2

--- 2018/rust/day14/src/main.rs
+++ 2018/rust/day14/src/main.rs
@@ -1,6 +1,5 @@
 use std::env;
-
-const input: usize = 110_201;
+use std::process;
 
 fn digits(n: u8) -> Vec<u8> {
     let mut digits = Vec::new();
@@ -19,7 +18,7 @@ fn part1() {
     let mut elf_2 = 1;
 
     loop {
-        if scoreboard.len() == input + 10 {
+        if scoreboard.len() == 110_201 + 10 {
             break;
         }
 
@@ -33,12 +32,43 @@ fn part1() {
         elf_2 = (elf_2 + 1 + elf_2_score as usize) % scoreboard.len();
     }
 
-    let result = &scoreboard[input..=input + 9].iter().map(|n| n.to_string()).collect::<String>();
+    let result = &scoreboard[110_201..=110_210]
+        .iter()
+        .map(|n| n.to_string())
+        .collect::<String>();
     println!("{}", result);
 }
 
 fn part2() {
+    let mut scoreboard: Vec<u8> = vec![3, 7];
+    let mut elf_1 = 0;
+    let mut elf_2 = 1;
 
+    loop {
+        let elf_1_score = scoreboard[elf_1];
+        let elf_2_score = scoreboard[elf_2];
+
+        let sum = elf_1_score + elf_2_score;
+        for digit in digits(sum).iter() {
+            scoreboard.push(*digit);
+
+            let len = scoreboard.len();
+            if len >= 6
+                && scoreboard[len - 6] == 1
+                && scoreboard[len - 5] == 1
+                && scoreboard[len - 4] == 0
+                && scoreboard[len - 3] == 2
+                && scoreboard[len - 2] == 0
+                && scoreboard[len - 1] == 1
+            {
+                println!("{}", len - 6);
+                process::exit(0);
+            }
+        }
+
+        elf_1 = (elf_1 + 1 + elf_1_score as usize) % scoreboard.len();
+        elf_2 = (elf_2 + 1 + elf_2_score as usize) % scoreboard.len();
+    }
 }
 
 fn main() {
