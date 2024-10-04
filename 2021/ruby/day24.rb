4085d20a2d61ee2b3bea76161700bf5fb94955a6
Rathesan Iyadurai <rad.iyadurai@gmail.com>
2024-10-04 13:52:51 +0200

    2022 day 5

---
+++ 2021/ruby/day24.rb
@@ -0,0 +1,324 @@
+def alu(*inputs)
+  instructions = DATA.readlines.map { |line| line.chomp.split }
+
+  vars = {
+    'w' => 0,
+    'x' => 0,
+    'y' => 0,
+    'z' => 0
+  }
+
+  instructions.each do |i|
+    case i
+    in ['inp', var]
+      vars[var] = inputs.shift
+    in ['add', left, right]
+      l = vars[left]
+      r = vars[right] || right.to_i
+      vars[left] = l + r
+    in ['mul', left, right]
+      l = vars[left]
+      r = vars[right] || right.to_i
+      vars[left] = l * r
+    in ['div', left, right]
+      l = vars[left]
+      r = vars[right] || right.to_i
+      vars[left] = l / r
+    in ['mod', left, right]
+      l = vars[left]
+      r = vars[right] || right.to_i
+      vars[left] = l % r
+    in ['eql', left, right]
+      l = vars[left]
+      r = vars[right] || right.to_i
+      l == r ? vars[left] = 1 : vars[left] = 0
+    end
+  end
+
+  puts vars
+  vars['z']
+end
+
+def decrement_model_number(number)
+  digits = number.to_s.chars
+  index = digits.size - 1
+
+  loop do
+    digit = digits[index].to_i
+
+    if digit == 1
+      index -= 1
+      next
+    else
+      digit -= 1
+      digits[index] = digit
+      return digits.join.to_i
+    end
+  end
+end
+
+number = 99999999999999
+
+loop do
+  z = alu number
+  if z.zero?
+    puts number
+    exit
+  end
+
+  number = decrement_model_number number
+end
+
+__END__
+inp w
+mul x 0
+add x z
+mod x 26
+div z 1
+add x 11
+eql x w
+eql x 0
+mul y 0
+add y 25
+mul y x
+add y 1
+mul z y
+mul y 0
+add y w
+add y 6
+mul y x
+add z y
+inp w
+mul x 0
+add x z
+mod x 26
+div z 1
+add x 11
+eql x w
+eql x 0
+mul y 0
+add y 25
+mul y x
+add y 1
+mul z y
+mul y 0
+add y w
+add y 14
+mul y x
+add z y
+inp w
+mul x 0
+add x z
+mod x 26
+div z 1
+add x 15
+eql x w
+eql x 0
+mul y 0
+add y 25
+mul y x
+add y 1
+mul z y
+mul y 0
+add y w
+add y 13
+mul y x
+add z y
+inp w
+mul x 0
+add x z
+mod x 26
+div z 26
+add x -14
+eql x w
+eql x 0
+mul y 0
+add y 25
+mul y x
+add y 1
+mul z y
+mul y 0
+add y w
+add y 1
+mul y x
+add z y
+inp w
+mul x 0
+add x z
+mod x 26
+div z 1
+add x 10
+eql x w
+eql x 0
+mul y 0
+add y 25
+mul y x
+add y 1
+mul z y
+mul y 0
+add y w
+add y 6
+mul y x
+add z y
+inp w
+mul x 0
+add x z
+mod x 26
+div z 26
+add x 0
+eql x w
+eql x 0
+mul y 0
+add y 25
+mul y x
+add y 1
+mul z y
+mul y 0
+add y w
+add y 13
+mul y x
+add z y
+inp w
+mul x 0
+add x z
+mod x 26
+div z 26
+add x -6
+eql x w
+eql x 0
+mul y 0
+add y 25
+mul y x
+add y 1
+mul z y
+mul y 0
+add y w
+add y 6
+mul y x
+add z y
+inp w
+mul x 0
+add x z
+mod x 26
+div z 1
+add x 13
+eql x w
+eql x 0
+mul y 0
+add y 25
+mul y x
+add y 1
+mul z y
+mul y 0
+add y w
+add y 3
+mul y x
+add z y
+inp w
+mul x 0
+add x z
+mod x 26
+div z 26
+add x -3
+eql x w
+eql x 0
+mul y 0
+add y 25
+mul y x
+add y 1
+mul z y
+mul y 0
+add y w
+add y 8
+mul y x
+add z y
+inp w
+mul x 0
+add x z
+mod x 26
+div z 1
+add x 13
+eql x w
+eql x 0
+mul y 0
+add y 25
+mul y x
+add y 1
+mul z y
+mul y 0
+add y w
+add y 14
+mul y x
+add z y
+inp w
+mul x 0
+add x z
+mod x 26
+div z 1
+add x 15
+eql x w
+eql x 0
+mul y 0
+add y 25
+mul y x
+add y 1
+mul z y
+mul y 0
+add y w
+add y 4
+mul y x
+add z y
+inp w
+mul x 0
+add x z
+mod x 26
+div z 26
+add x -2
+eql x w
+eql x 0
+mul y 0
+add y 25
+mul y x
+add y 1
+mul z y
+mul y 0
+add y w
+add y 7
+mul y x
+add z y
+inp w
+mul x 0
+add x z
+mod x 26
+div z 26
+add x -9
+eql x w
+eql x 0
+mul y 0
+add y 25
+mul y x
+add y 1
+mul z y
+mul y 0
+add y w
+add y 15
+mul y x
+add z y
+inp w
+mul x 0
+add x z
+mod x 26
+div z 26
+add x -2
+eql x w
+eql x 0
+mul y 0
+add y 25
+mul y x
+add y 1
+mul z y
+mul y 0
+add y w
+add y 1
+mul y x
+add z y
---
+++ 2022/python/README.md
@@ -0,0 +1,5 @@
+### Running a lab server
+
+```sh
+jupyter lab
+```
